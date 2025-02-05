35bc8bd71da91a1cdd843ed4b17d0f3058dbaf64
Sakshi Verma <sakshi02v@gmail.com>
2025-02-05 13:09:43 +0530

    [Sakshi Verma]Sorting Job Applicants by salary using Heap Sort Algo

---
+++ heapsort_sortjobapplicantbysalary/Main.java
@@ -0,0 +1,20 @@
+package heapsort_sortjobapplicantbysalary;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // example salary expected
+        int[] salaryDemands = {8100, 8900, 900, 1100, 1400, 7600, 5400, 2200};
+
+        // printing the unsorted array
+        System.out.println(Arrays.toString(salaryDemands));
+
+        // sorting the salary demands
+        SortJobApplicantsBySalary.maxHeap(salaryDemands);
+
+        // printing the update salary demands array
+        System.out.println(Arrays.toString(salaryDemands));
+    }
+
+}
